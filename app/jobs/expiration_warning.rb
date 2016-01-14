3e8a126a63cc81dc85bf5bd31726a1277b37bec6
Joe Buhlig <joe@joebuhlig.com>
2016-01-14 15:24:21 -0600

    Added execution functions for sidekiq

--- app/jobs/expiration_warning.rb
+++ app/jobs/expiration_warning.rb
@@ -1,9 +1,18 @@
 module DiscoursePremiumBt
 	class ExpirationWarning < ::Jobs::Scheduled
-		every 1.minute
+		every 1.hour
 
 		def execute(args)
-
+			group = Group.find_by_name(SiteSetting.premium_bt_group_name)
+			group.users.each do |user|
+				if user.custom_fields['premium_exp_date'] < Time.now + 40.days and user.custom_fields['premium_exp_pm_sent'] == false
+					title = I18n.t("premium_bt.pm_warning_title")
+					raw = I18n.t("premium_bt.pm_warning_text")
+					user.send_premium_pm(title, raw)
+					user.custom_fields['premium_exp_pm_sent'] = true
+					user.save
+				end
+			end
 		end
 	end
 end
--- app/jobs/free_month_expiration.rb
+++ app/jobs/free_month_expiration.rb
@@ -1,9 +1,19 @@
 module DiscoursePremiumBt
 	class FreeMonthExpiration < ::Jobs::Scheduled
-		every 1.minute
+		every 1.hour
 
 		def execute(args)
-
+			group = Group.find_by_name(SiteSetting.premium_bt_group_name)
+			group.users.each do |user|
+				if user.custom_fields['premium_exp_date'] < Time.now + 40.days
+					user.premium_group_revoke
+					title = I18n.t("premium_bt.pm_expired_title")
+					raw = I18n.t("premium_bt.pm_expired_text")
+					user.send_premium_pm(title, raw)
+					user.custom_fields['premium_exp_pm_sent'] = true
+					user.save
+				end
+			end
 		end
 	end
 end
